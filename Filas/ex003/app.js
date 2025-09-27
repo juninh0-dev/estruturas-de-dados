d0a00aa82a10c0f3ab66dedb18b111110c92c7ee
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-09-26 21:52:10 -0300

    ex003 de Filas terminado com sucesso, indo para o ex004

---
+++ Filas/ex003/app.js
@@ -0,0 +1,17 @@
+const Fila = require('./fila');
+
+const bilhete = new Fila();
+
+bilhete.enqueue("Visitante 1");
+bilhete.enqueue("Visitante 2");
+bilhete.enqueue("Visitante 3");
+bilhete.enqueue("Visitante 4");
+
+let contador = 1;
+
+while(!bilhete.estaVazia()){
+    console.log(`O visitante ${contador}, embarcou no brinquedo.`);
+    let desembarque =  bilhete.dequeue();
+    console.log(`O visitante ${desembarque}, desembarcou no brinquedo.`);
+    contador++;
+}
