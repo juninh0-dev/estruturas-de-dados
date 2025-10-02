e55f65fb0a9102b85b6e02b1e1ab74ccff6831ad
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-10-02 12:46:50 -0300

    Corrigindo bugs e ajeitando o terminal em app.js

--- ListaEncadeada/teste/LinkedList_prototipo.js
+++ ListaEncadeada/teste/LinkedList_prototipo.js
@@ -1,112 +1,19 @@
 class LinkedList{
-    #head = undefined
-    #length = 0;
-    // Inserir no início da Lista
-    insertAtBegining(value){ 
-        
-        //Cria um novo nó
-        let newNode = new Node(value);
 
-        // O novo nó aponta para o antigo "head"
-        newNode.next = this.#head;
+    insertAtBeginning(value) {   }
 
-        // Atualiza o "head" para o novo nó
-        this.#head = newNode;
-
-        // Incrementa o tamanho da lista
-        this.#length++;
-    }
+    insertAtEnd(value) { }
     
-    //Inserir no fim da lista
-    insertAtEnd(value){ 
-        let newNode = new Node(value);
-        
-        // Se a lista estiver vazia, o novo nó se torna o "head"
-        if(this.#head === undefined){
-            this.head = newNode;
-            return;
-        }
-
-        // Percorre até o ultimo nó
-        let current = this.#head;
-        while(current.next !== undefined){
-            current = current.next;
-        }
-
-        // Faz o último nó apontar para o novo nó
-        current.next = newNode;
-        
-        this.next;
+    removeByValue(value) {   }
     
-    }
+    find(value) {    }
+    
+    size() { }
+    
+    isEmpty() {  }
+    
+    toString() { }
 
-    // Remover um nó por vlaor
-    removeByValue(value){ 
-        // A lista está vazia
-        if(this.#head === undefined){
-            return;
-        }
-
-        // Se o valor a ser removido estiver no primeiro nó
-        if(this.#head.value === value){
-
-            //Atualiza o "head" para o próximo nó
-            this.#head = this.#head.next;
-        }
-
-        // Percorre a lista procurando o nó a ser removido
-        let current = this.#head;
-        while (current.next !== undefined && current.next.value !== value) {
-            current = current.next.next;
-        }
-
-        // Se o nó foi encontrado, ajusta o ponteiro para pular o nó removido
-        if(current.next !== undefined) {
-            current.next = current.next.next;
-        }
-
-        this.#length--;
-    }
-
-    // Buscar um nó por valor
-    find(value){ 
-        let current = this.#head;
-
-        // Percorre a lista até encontrar o nó ou até o final
-        while (current !== undefined){
-            console.log(current.value);
-            if(current.value === value) {
-                return current; // Não encontrado
-            }
-            current = current.next;
-        }
-        return undefined; // Se não encontrar retorna undefined
-    }
-    size(){ 
-        // Retorna o tamanho da lista
-
-        let count = 0;
-        let current = this.#head;
-        while (current !== undefined){
-            count++;
-            current = current.next;
-        }
-
-        return count;
-    }
-    // Retorna true se a lista estiver vazia
-    isEmpty = () => this.#head === undefined;
-
-    // Exibir a lista (opcional, para facilitar a visualização)
-    toString(){ 
-        let current = this.#head;
-        let list = '';
-        while(current !== undefined) {
-            list += current.value + ' -> ';
-            current = current.next;
-        }
-        console.log(list + 'undefined');
-    }
 }
 
 module.exports = LinkedList;
