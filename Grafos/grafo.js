58a1d86dbf142fb9347cc66ace1dfaffed54ab09
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-10-27 12:09:45 -0300

    Fazendo o protótipo da estrutura grafo e entendendo como funciona.

--- Grafos/grafo.js
+++ Grafos/grafo.js
@@ -1,6 +1,28 @@
 class Grafo {
     constructor(){
-        
+
+    }
+    // Adiciona um novo vértice ao grafo
+    adicionnarVertice(vertice){
+
     }
 
+    // Adiciona uma aresta entre dois vértices (não dirigido).
+    adicionnarAresta(vertice1, vertice2){
+
+    }
+
+    // Remove uma aresta entre dois vértices.
+    removerAresta(vertice1, vertice2){
+    }
+
+    // Remove um vértice e suas conexões
+    removerVertice(vertice){
+
+    }
+
+    //Exibe o grafo>
+    imprimirGrafo(){
+        
+    }
 }
