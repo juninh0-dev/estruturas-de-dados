7c3ccaeb5f27c40a48ff0dea7bf863d1786d9a43
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-11-23 12:40:18 -0300

    Implementação do BubbleShort feita.

--- Alg. De Ordenação/BubbleShort/shorter.js
+++ Alg. De Ordenação/BubbleShort/shorter.js
@@ -3,4 +3,23 @@ class Shorter {
      * Bubble Short
      * 
     */
+
+    static bubbleShort(arr){
+        const array = [...arr]; // copia para não alterar o original
+        let n = array.length;
+        let trocou;
+        
+        do {
+           trocou = false;
+           for(let i = 0; i< n - 1; i++){
+            if(array[i] > array[i = 1]){
+                [array[i], array[i + 1]] = [array[i + 1], array[i + 1]];
+                trocou = true;
+            }
+           }
+           n--;
+        } while (trocou);
+        
+        return array;
+    }
 }
